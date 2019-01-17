95ba7b475d76ab3052df4a9e069e82426b2b42f6
franeyjr <franeyjr@rose-hulman.edu>
2019-01-17 16:58:40 -0500

    NOT DONE

--- src/m4_nested_loops_in_sequences.py
+++ src/m4_nested_loops_in_sequences.py
@@ -117,7 +117,7 @@ def multiply_numbers(sequence_of_lists):
        [FYI: This 'can be multiplied ...' is an example of DUCK TYPING.]
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
@@ -129,13 +129,13 @@ def multiply_numbers(sequence_of_lists):
     #        -- See   m2e_nested_loops_in_sequences   as needed.
     # ------------------------------------------------------------------
     for j in range(len(sequence_of_lists)):
-        for k in range(j+1):
-            sequence_of_lists[j] = sequence_of_lists[j] * [k]
+        for k in range(len(sequence_of_lists[j])):
+            sequence_of_lists[j][k] = sequence_of_lists[j][k] * (j+1)
 
 def run_test_sum_numbers():
     """ Tests the    sum_numbers    function. """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this TEST function.
+    # DONE: 3. Implement this TEST function.
     #   It TESTS the  sum_numbers  function defined below.
     #   Include at least **   3   ** tests (we wrote two for you).
     # ------------------------------------------------------------------
@@ -156,6 +156,20 @@ def run_test_sum_numbers():
 
     # TO DO 3 (continued): Add your ADDITIONAL test(s) here:
 
+    # Test 3:
+    expected = 51
+    answer = sum_numbers(([1,6,8], [5], [12,5,14]))
+    print('Expected and actual are:', expected, answer)
+
+    # Test 4:
+    expected = 37
+    answer = sum_numbers(([], [5,3,7,5,2], [1,2,3,4,5]))
+    print('Expected and actual are:', expected, answer)
+
+    # Test 5:
+    expected = 64
+    answer = sum_numbers(([1,5,3,7,9], [5], [11,17,4,2]))
+    print('Expected and actual are:', expected, answer)
 
 def sum_numbers(seq_seq):
     """
@@ -168,10 +182,14 @@ def sum_numbers(seq_seq):
                     and each item in the subsequences is a number.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
-
+    sum = 0
+    for j in range(len(seq_seq)):
+        for k in range(len(seq_seq[j])):
+            sum = sum + seq_seq[j][k]
+    return sum
 
 def run_test_print_characters():
     """ Tests the    print_characters    function. """
@@ -219,11 +237,13 @@ def print_characters(sequence_of_strings):
     Precondition:  the given argument is a sequence of strings.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     # ------------------------------------------------------------------
-
+    for j in range(len(sequence_of_strings)):
+        for k in range(len(sequence_of_strings[j])):
+            print(sequence_of_strings[j][k])
 
 def run_test_print_characters_slanted():
     """ Tests the    print_characters_slanted    function. """
@@ -279,7 +299,9 @@ def print_characters_slanted(sequence_of_strings):
     # ** HINT: ** Consider using string multiplication for the spaces
     #             and string addition to stitch the spaces to the character.
     # ------------------------------------------------------------------
-
+    for j in range(len(sequence_of_strings)):
+        for k in range(len(sequence_of_strings[j])):
+            print(sequence_of_strings[j][k])
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
